059e63d01ed7d8fdc880d7200fb968430c7b284c
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2022-09-08 16:01:01 +0300

    admin

--- FE/geo-guesser-group-project/src/App.js
+++ FE/geo-guesser-group-project/src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Admin from "./pages/Admin";
 
 function App() {
+  const [isAdmin, setisAdmin] = useState(false);
+  
+
+
+
+
+
+
   return (
-    <div>App</div>
+    <div className='app-wrapper'>
+<BrowserRouter>
+<Routes>
+<Route path="/admin" element={<Admin/>}/>
+
+</Routes>
+
+</BrowserRouter>
+
+
+
+    </div>
   )
 }
 
---
+++ FE/geo-guesser-group-project/src/pages/Admin.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function Admin() {
+  const [locationImage, setLocationImageImage] = useState();
+  const [answers, setaAswers] = uAnState({});
+
+  return (
+  <div className="admin-container">
+
+
+    
+  </div>
+
+
+  )
+};
+
+export default Admin;
