144665379b87784d433e86385c0900acb854def7
Jonathan Cohen <96384721+JoniCohen@users.noreply.github.com>
2022-09-11 15:54:14 +0300

    Queries highscore

--- BE/controllers/usersControllers.js
+++ BE/controllers/usersControllers.js
@@ -4,7 +4,8 @@ async function insertNewPoints(req,res){
   try{
     const {nameUser,level, points} = req.body
     const nameUserUpper = nameUser.toUpperCase()
-    const newPoints = await insertNewPointsModel(nameUserUpper, level, points)
+    const levelUpper = level.toUpperCase()
+    const newPoints = await insertNewPointsModel(nameUserUpper, levelUpper, points)
     res.send(newPoints)
   }catch(err){
     res.status(500).send(err)
