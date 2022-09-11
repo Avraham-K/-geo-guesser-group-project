863ed8b82f0f2977c687640b6ec330d0bcd00e21
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2022-09-11 11:06:12 +0300

    admin page

--- FE/geo-guesser-group-project/src/pages/Admin.jsx
+++ FE/geo-guesser-group-project/src/pages/Admin.jsx
@@ -1,42 +1,68 @@
 import React, { useState } from "react";
-import { Input } from '@chakra-ui/react'
+import { Form, Col, Row } from "react-bootstrap";
+import axios from "axios";
 
 function Admin() {
-  const [locationImage, setLocationImageImage] = useState();
+  const [Image, setImage] = useState();
+  const [location, setLocation] = useState();
   const [answers, setaAswers] = useState({});
   const [isAdmin, setisAdmin] = useState(false);
 
+  const handleImage = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+
+      const formData = new FormData();
+      formData.append("location", location);
+      formData.append("locationImage", Image);
+
+      const res = await axios.post("http://localhost:8080/admin", formData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-  <div className="admin-container">
-    <h1>Admin Page</h1>
-    <Input placeholder='Basic usage' />
+    <div className="admin-container">
+      <h1 className="display-3">Admin Page</h1>
+      <Form className="admin-add-pet-form">
+        <Row className="mb-3">
+          <Form.Group as={Col} className="mb-3" controlId="formBasicname">
+            <Form.Label>Location</Form.Label>
+            <Form.Control
+              name="location"
+              onChange={(e) => setLocation(e.target.value)}
+              type="text"
+              placeholder="Location"
+            />
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formImageFile" className="mb-3">
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              type="file"
+              accept="image/*"
+              name="image"
+              onChange={handleImage}
+            />
+          </Form.Group>
+        </Row>
 
-    <FilePicker
-    onFileChange={(fileList) => { }}
-    placeholder="placeholder"
-    clearButtonLabel="label"
-    multipleFiles={true}
-    accept="application/json"
-    hideClearButton={false}
-    ref={myRef}
-/>
-
-    {/* <FileUpload
-            accept={'image/*'}
-            multiple
-            register={register('file_', { validate: validateFiles })}
-          >
-            <Button leftIcon={<Icon as={FiFile} />}>
-              Upload
-            </Button>
-          </FileUpload> */}
-
-
-  </div>
-
-
-  )
-};
+        <button
+          className="add-location-button"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Add Location
+        </button>
+      </Form>
+    </div>
+  );
+}
 
 export default Admin;
